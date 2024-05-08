d79d4d38a6758e5de319871a87d685f50c4493fb
Alexandra Farah <alexandra.farah@uri.edu>
2024-05-08 00:26:55 -0400

    final edits

--- test_q3.py
+++ test_q3.py
@@ -5,7 +5,8 @@ import pytest
 
 def test_empty_file(): #Testing if there was an empty file inserted what error would happen. 
   file = "empty.txt" #This file has an empty text
-  assert find_smallest_k(file) == NONE #it should come back with nothing
+  k = 4 #a value for k
+  assert find_smallest_k(file) == NONE #it should come back with nothing. If it matches this will pass
   
 def test_error_no_file(): #Testing what would happen if there wasn't a file given
   with pytest.raises(FileNotFoundError): #an error should arise where there isn't a file found/possible
@@ -14,5 +15,6 @@ def test_error_no_file(): #Testing what would happen if there wasn't a file give
     
 def not_sequences_just_words(): #Testing what would happen if the file had something in it, but not sequences
   file = "nosequence.txt" #This file has words in it, but not a sequence. It also has symbols. 
-  assert find_smallest_k(file) == NONE #The result should be no value for k.
+  k = 4 #a value for k
+  assert find_smallest_k(file) == NONE #The result should be no value for k. If it matches this will pass. 
   
