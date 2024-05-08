9c0c4968a42966490e911650ca4c89a7b5cd0323
Alexandra Farah <alexandra.farah@uri.edu>
2024-05-08 00:08:26 -0400

    update again

--- test_q2.py
+++ test_q2.py
@@ -4,21 +4,20 @@
 from exam4_q2 import get_geno_strings, all_substrings
 import pytest
 
-def test_k_zero_pt2():
-  file = "sample.txt"
-  k = 0
-  CORRECT = {}
-  assert all_substrings(file, k) == CORRECT
+def test_k_zero_pt2(): #Testing what would happen if k was 0. 
+  file = "sample.txt" #The sample text to use.
+  k = 0 #the input for k
+  CORRECT = {'AGTCGTGCAGTCAGTG': {'': {''}}, 'GTACGTACGATCACGAGAG': {'': {''}}, 'ACGGAC': {'': {''}}} #The output that is expected when k = 0
+  assert all_substrings(file, k) == CORRECT #If this matches the function passes. 
   
-def test_empty_file():
-  file = "empty.txt"
-  k = 3
-  all_substrings(file, k)
-  CORRECT = {}
-  assert all_substrings(file, k) == CORRECT
+def test_empty_file(): #Testing what would happen if an empty file was given. 
+  file = "empty.txt" #The file is empty, but it exists.
+  k = 3 #A value for k
+  CORRECT = {'': {}} #What the expected outcome is
+  assert all_substrings(file, k) == CORRECT #If this matches the function passes. 
 
-def test_missing_file():
-  with pytest.raises(FileNotFoundError):
-    file = "missing"
-    k = 4
-    all_substrings(file, k)
+def test_missing_file(): #Testing what would happen if there was no file given
+  with pytest.raises(FileNotFoundError): #An error would come up saying file is not found. 
+    file = "missing.txt" #The missing file!
+    k = 4 #A value for k
+    all_substrings(file, k) #the function. 
