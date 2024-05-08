2426a12549791360004ba8fc85d75e8da91ea891
Alexandra Farah <alexandra.farah@uri.edu>
2024-05-07 23:39:54 -0400

    updated

--- test_q3.py
+++ test_q3.py
@@ -3,16 +3,16 @@
 from exam4_q3 import find_smallest_k, all_substrings, get_geno_strings
 import pytest
 
-def test_empty_file():
-  file = "empty.txt"
-  assert find_smallest_k(file) == NO K
+def test_empty_file(): #Testing if there was an empty file inserted what error would happen. 
+  file = "empty.txt" #This file has an empty text
+  assert find_smallest_k(file) == NONE #it should come back with nothing
   
-def test_error_no_file():
-  with pytest.raises(FileNotFoundError)
-    file = "missing.txt"
-    find_smallest_k(file)
+def test_error_no_file(): #Testing what would happen if there wasn't a file given
+  with pytest.raises(FileNotFoundError): #an error should arise where there isn't a file found/possible
+    file = "missing.txt" #The file is missing
+    find_smallest_k(file) #The function
     
-def not_sequences_just_words():
-  file = "nosequence.txt"
-  assert find_smallest_k(file) == NO SEQUENCE
+def not_sequences_just_words(): #Testing what would happen if the file had something in it, but not sequences
+  file = "nosequence.txt" #This file has words in it, but not a sequence. It also has symbols. 
+  assert find_smallest_k(file) == NONE #The result should be no value for k.
   
